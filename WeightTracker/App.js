f402e6a69a501b206e807cc7d8f5e33ffccdeea7
Adam Parr <adamparr.uk@gmail.com>
2023-01-21 17:20:39 +0000

    add chart

--- WeightTracker/App.js
+++ WeightTracker/App.js
@@ -1,36 +1,107 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Platform, StyleSheet, Text, View, Button, TextInput } from 'react-native';
-
+import { Platform, StyleSheet, Text, View, Button, TextInput,ScrollView, Dimensions } from 'react-native';
+import { LineChart } from 'react-native-chart-kit';
 
 export default function App() {
   
   const [weight, setWeight] = useState()
-  const [weightsList, setWeightsList] = useState([])
+  const [weightsList, setWeightsList] = useState([{date: new Date(), weight: 50}])
+
+
+  
+  const weightX = weightsList.map(weightItem => weightItem.date)
+  const weightY = weightsList.map(weightItem => weightItem.weight)
+
+  console.log(weightX, weightY)
+
+  const chartData = {
+    labels: weightX,
+    datasets:[{
+      data: weightY,
+      color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})` // optional
+    }]
+  }
+  const testData = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+    datasets: [{
+      data: [
+        50,
+        20,
+        2,
+        86,
+        71,
+        100
+      ],
+      color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})` // optional
+    }]
+  }
+
+  const testData2 = {
+    labels: weightX,
+    datasets: [{
+      data: weightY,
+      color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})` // optional
+    }]
+  }
 
   const handlePress = (event) => {
     const weightData = { date: new Date(), weight: weight }
     console.log(weightData)
-    setWeightsList(prevWeightsList => [weightData, ...prevWeightsList])
+    setWeightsList(prevWeightsList => [...prevWeightsList, weightData])
     console.log(weightsList)
     setWeight()
   }
 
   return (
     <View style={styles.container}>
-      <Text>Enter your weight</Text>
-      <TextInput
-        style={{height: 40}}
-        placeholder="Enter weight in kg"
-        onChangeText={newWeight => setWeight(newWeight)}
-        defaultValue={weight}
-      />
-      <Text>Your weight is {weight}. Boom boom</Text>
-      <Button
-        title="Add"
-        onPress={handlePress}
-      />
-      {weightsList && weightsList.map(weightItem => (<Text>{weightItem.date.toLocaleString()}: {weightItem.weight}</Text>))}
+      <View>
+        <Text>Enter your weight</Text>
+        <TextInput
+          style={{height: 40, borderWidth:0.5, borderRadius:8}}
+          placeholder="Enter weight in kg"
+          onChangeText={newWeight => setWeight(newWeight)}
+          defaultValue={weight}
+        />
+        <Button
+          title="Add"
+          onPress={handlePress}
+        />
+
+        <Text>Your weight is {weight}. Boom boom</Text>
+        <View style={styles.container}>
+        <LineChart
+          verticalLabelRotation={90}
+          data={testData2}
+          width={Dimensions.get("window").width}
+          height={300}
+          yAxisSuffix="kg"
+          chartConfig={{
+            backgroundColor: "#e26a00",
+            backgroundGradientFrom: "#fb8c00",
+            backgroundGradientTo: "#ffa726",
+            decimalPlaces: 2, // optional, defaults to 2dp
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            style: {
+              borderRadius: 16
+            },
+            propsForDots: {
+              r: "6",
+              strokeWidth: "2",
+              stroke: "#ffa726"
+            }
+          }}
+          bezier
+          style={{
+            marginVertical: 8,
+            borderRadius: 16,
+          }}
+        />
+        </View>
+      </View>
+
+      {weightsList && weightsList.map(weightItem => (<Text key={weightItem.date.toLocaleString()}>{weightItem.date.toLocaleString()}: {weightItem.weight}</Text>))}
       <StatusBar style="auto" />
     </View>
   );
--- todos/todos.md
+++ todos/todos.md
@@ -1,6 +1,11 @@
-1. Create entry form on main page for weights
-2. Store form data in localstorage (JSON)
-3. Create line chart on main page using JSON data from localstorage
+- [X] Create entry form on main page for weights
+- [X] Create line chart on main page
+- [ ] Store form data in localstorage (JSON)
+- [ ] Refactor to components
+- [ ] Update chart to use localstorage
+- [ ] Style
+- [ ] Data validation
+- [ ] Push notifications to remind user to weigh themselves
 
 
 https://docs.expo.dev/versions/v47.0.0/sdk/date-time-picker/
